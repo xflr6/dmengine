92bada10d76d3aa32d515bfd210c1ae61f1c1c90
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-05-01 09:34:00 +0200

    switch from nose to pytest

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,25 +1,27 @@
 # test_features.py
 
-import unittest
+import pytest
 
 from dmengine.features import FeatureSystem
 
 
-class TestFeatureSystem(unittest.TestCase):
+@pytest.fixture(scope='session')
+def fs():
+    features = [
+        {'value': '+1', 'category': 'person'},
+        {'value': '-1', 'category': 'person'},
+        {'value': '+2', 'category': 'person'},
+        {'value': '-2', 'category': 'person'},
+        {'value': '+3', 'category': 'person'},
+        {'value': '-3', 'category': 'person'},
+        {'value': '+sg', 'category': 'number'},
+        {'value': '+pl', 'category': 'number'},
+        {'value': '-sg', 'category': 'number'},
+        {'value': '-pl', 'category': 'number'},
+    ]
+    return FeatureSystem(features)
 
-    def test_init(self):
-        fs = FeatureSystem([
-            {'value': '+1', 'category': 'person'},
-            {'value': '-1', 'category': 'person'},
-            {'value': '+2', 'category': 'person'},
-            {'value': '-2', 'category': 'person'},
-            {'value': '+3', 'category': 'person'},
-            {'value': '-3', 'category': 'person'},
-            {'value': '+sg', 'category': 'number'},
-            {'value': '+pl', 'category': 'number'},
-            {'value': '-sg', 'category': 'number'},
-            {'value': '-pl', 'category': 'number'},
-        ])
-        self.assertEqual(
-            [(f.value, f.category) for f in fs.FeatureSet('+1 +sg').features],
-            [('+1', 'person'), ('+sg', 'number')])
+
+def test_init(fs):
+    assert [(f.value, f.category) for f in fs.FeatureSet('+1 +sg').features] == \
+           [('+1', 'person'), ('+sg', 'number')]
