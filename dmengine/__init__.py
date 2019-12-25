4827976a118f6cbd50d365447eaf06268f4797cc
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2019-12-25 12:55:56 +0100

    bump version for development

--- dmengine/__init__.py
+++ dmengine/__init__.py
@@ -10,7 +10,7 @@ from .reporting import Report, texify
 __all__ = ['Analysis', 'Report', 'calculate', 'texify']
 
 __title__ = 'dmengine'
-__version__ = '0.2.7'
+__version__ = '0.2.8.dev0'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE.txt'
 __copyright__ = 'Copyright (c) 2011-2019 Sebastian Bank'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dmengine',
-    version='0.2.7',
+    version='0.2.8.dev0',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Distributed Morphology calculator',
