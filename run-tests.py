ced34c49ccad6decb5b3d7a94f145322a287cc43
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-01-03 19:06:53 +0100

    drop PY2 branch from test-running script

--- run-tests.py
+++ run-tests.py
@@ -12,7 +12,5 @@ ARGS = [
 
 if 'idlelib' in sys.modules:
     ARGS.extend(['--capture=sys', '--color=no'])
-elif sys.version_info.major == 2 and 'win_unicode_console' in sys.modules:
-    ARGS.append('--capture=sys')
 
 sys.exit(pytest.main(ARGS + sys.argv[1:]))
