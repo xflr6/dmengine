08ec9115d140824e2883f670c92969d901bf9d9b
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-12-05 13:43:42 +0100

    increase verbosity

--- try-examples.py
+++ try-examples.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 import glob
+import pathlib
+import sys
 
 import dmengine
+
+SELF = pathlib.Path(__file__)
 
 EXAMPLES = 'examples/*.yaml'
 
@@ -11,5 +15,8 @@ DIRECTORY = 'examples-output'
 PDF = True
 
 
+print('run', [SELF.name] + sys.argv[1:])
 for filename in glob.glob(EXAMPLES):
+    print()
+    print(f'dmengine.calculate({filename!r}, directory={DIRECTORY!r}, pdf={PDF!r})')
     analysis = dmengine.calculate(filename, directory=DIRECTORY, pdf=PDF)
