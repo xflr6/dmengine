69d11f90723c37940dffc3d8da28c40970a31fb5
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-12-05 13:03:04 +0100

    increase verbosity

--- run-tests.py
+++ run-tests.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
-# run-tests.py
 
+"""Run the tests with https://pytest.org."""
+
+import pathlib
 import sys
 
 import pytest
 
-ARGS = [
-    #'--exitfirst',
-    #'--pdb',
-]
+SELF = pathlib.Path(__file__)
+
+ARGS = [#'--verbose',
+        #'--pdb',
+        #'--exitfirst',  # a.k.a. -x
+        #'-W', 'error',
+       ]
 
 if 'idlelib' in sys.modules:
-    ARGS.extend(['--capture=sys', '--color=no'])
+    ARGS += ['--capture=sys', '--color=no']
 
-sys.exit(pytest.main(ARGS + sys.argv[1:]))
+print('run', [SELF.name] + sys.argv[1:])
+args = ARGS + sys.argv[1:]
+
+print(f'pytest.main({args!r})')
+sys.exit(pytest.main(args))
