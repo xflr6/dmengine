229545534242ac814a8f5c94d31c4b2894be67c7
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2025-12-03 21:27:13 +0100

    add test coverage for command-line interface entry point
    - fails due to bug introduced in e8eb7431f5e0f4a4ae8e471b5c0614428e505e80

--- try-examples.py
+++ try-examples.py
@@ -3,6 +3,7 @@
 import glob
 import pathlib
 import sys
+import subprocess
 
 import dmengine
 
@@ -18,3 +19,9 @@ print('run', [pathlib.Path(__file__).name] + sys.argv[1:])
 for filename in glob.glob(EXAMPLES):
     print('', f'dmengine.calculate({filename!r}, directory={DIRECTORY!r}, pdf={PDF!r})', sep='\n')
     analysis = dmengine.calculate(filename, directory=DIRECTORY, pdf=PDF)
+
+    cmd = [sys.executable, '-m', 'dmengine', filename, DIRECTORY]
+    if PDF:
+        cmd.append('--pdf')
+    print('', f'subprocess.run({cmd!r})', sep='\n')
+    subprocess.run(cmd, check=True)

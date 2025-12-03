xflr6/dmengine

commit e8eb7431f5e0f4a4ae8e471b5c0614428e505e80

use keyword-only args and more block-style

try-examples.py
@@ -12,4 +12,4 @@ DIRECTORY = 'examples-output'
 PDF = True
 
 for filename in glob.glob(EXAMPLES):
-    analysis = dmengine.calculate(filename, DIRECTORY, pdf=PDF)
+    analysis = dmengine.calculate(filename, directory=DIRECTORY, pdf=PDF)

dmengine/__init__.py
@@ -19,9 +19,9 @@ __copyright__ = 'Copyright (c) 2011-2021 Sebastian Bank'
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 
-def calculate(filename, directory=None, report=False, pdf=False, view=False):
+def calculate(filename, *, directory=None, report=False, pdf=False, view=False):
     """Return calculated DM analysis from the given config filename."""
-    analysis = Analysis(filename, directory)
+    analysis = Analysis(filename, directory=directory)
     analysis.calculate()
     analysis.save()
 

dmengine/analysis.py
@@ -34,9 +34,12 @@ class Analysis(object):
 
     Calculator = calculation.Calculator
 
-    def __init__(self, filename, directory=None, encoding='utf-8'):
+    def __init__(self, filename, *, directory=None, encoding='utf-8'):
         self.filename = filename
-        self.results = tools.derive_filename(filename, '-results', 'yaml', directory)
+        self.results = tools.derive_filename(filename,
+                                             suffix='-results',
+                                             extension='yaml',
+                                             directory=directory)
 
         log.info(f'{self!r}')
 
@@ -74,7 +77,7 @@ class Analysis(object):
         log.info('\tcalculate..')
         self.worklog, self.outputs, self.spellouts = self.calculator()
 
-    def save(self, encoding='utf-8', newline=''):
+    def save(self, *, encoding='utf-8', newline=''):
         log.info(f'\tsave to {self.results!r}..')
 
         data = collections.OrderedDict([

dmengine/exponents.py
@@ -55,7 +55,7 @@ class Exponent(metaclass=ExponentMeta):
         if not form:
             raise ValueError(f'{self!r} empty form.')
 
-    def copy(self, form=None):
+    def copy(self, *, form=None):
         if form is None:
             form = self.form
         return self.__class__(form)

dmengine/features.py
@@ -66,7 +66,7 @@ class FeatureSetMeta(type):
 
     system = None
 
-    def __call__(self, values, sortkey=operator.attrgetter('index')):  # noqa: N804
+    def __call__(self, values, *, sortkey=operator.attrgetter('index')):  # noqa: N804
         if isinstance(values, self):
             return values.copy()
 
@@ -144,14 +144,14 @@ class FeatureSet(metaclass=FeatureSetMeta):
         return [f.value for f in self.features if f.visible]
 
     @property
-    def by_category(self, groupkey=operator.attrgetter('category')):
+    def by_category(self, *, groupkey=operator.attrgetter('category')):
         features = sorted(self.features, key=groupkey)
         mapping = {k: [f.value for f in g]
                    for k, g in groupby(features, groupkey)}
         return [mapping.get(c, []) for c in self.__class__.system.categories]
 
     @property
-    def by_specificity(self, groupkey=operator.attrgetter('specificity')):
+    def by_specificity(self, *, groupkey=operator.attrgetter('specificity')):
         features = sorted(self.features, key=groupkey)
         mapping = {k: [f.value for f in g]
                    for k, g in groupby(features, groupkey)}
@@ -161,7 +161,7 @@ class FeatureSet(metaclass=FeatureSetMeta):
         self._clearlazy()
         self.features |= other.features
 
-    def remove(self, other, discard=False):
+    def remove(self, other, *, discard=False):
         if not discard and not other.features <= self.features:
             raise KeyError
         self._clearlazy()
@@ -193,7 +193,7 @@ class FeatureBag(FeatureSet):
         self._clearlazy()
         self.features += other.features
 
-    def remove(self, other, discard=False):
+    def remove(self, other, *, discard=False):
         if not discard and not self.issubset(other):
             raise KeyError
         self._clearlazy()
@@ -243,7 +243,7 @@ class FeatureSystem(object):
                   for f in self.mapping.values()]
         return dumper.represent_sequence('tag:yaml.org,2002:seq', result)
 
-    def __init__(self, features_kwargs=(), always_bag=False):
+    def __init__(self, features_kwargs=(), *, always_bag=False):
         class Feature(self.Feature):
             __slots__ = ()
             system = self
@@ -320,11 +320,9 @@ class FeatureSystem(object):
         Feature.specificity = int(specificity)
         return Feature
 
-    _derive_replaces = [
-        [('+', 'plus'), ('-', 'minus')],
-        [('1', 'first'), ('2', 'second'), ('3', 'third')],
-        [('sg', 'singular'), ('du', 'dual'), ('pl', 'plural')],
-    ]
+    _derive_replaces = [[('+', 'plus'), ('-', 'minus')],
+                        [('1', 'first'), ('2', 'second'), ('3', 'third')],
+                        [('sg', 'singular'), ('du', 'dual'), ('pl', 'plural')]]
 
     @classmethod
     def derive_name(cls, value):

dmengine/outcontexts.py
@@ -43,7 +43,7 @@ class ViContexts(types.Instances):
 
     iteritems = items
 
-    def _kwstr(self, plain=False):
+    def _kwstr(self, *, plain=False):
         ctx = ', '.join(c._kwstr() for c in self)
         return ctx if plain else f', {ctx}' if ctx else ''
 

dmengine/readjustments.py
@@ -150,5 +150,5 @@ class TransformExponent(Readjustment):
             new_form, subbed = self._subn(self.replace, vi.exponent.form)
             applied = True
             if subbed:
-                vis[i] = vi.copy(new_form)
+                vis[i] = vi.copy(form=new_form)
         return applied

dmengine/reporting/__init__.py
@@ -7,7 +7,7 @@ from .report import Report
 __all__ = ['Report', 'texify']
 
 
-def texify(filename=None, pdf=False, view=False):
+def texify(filename=None, *, pdf=False, view=False):
     report = Report(filename)
     report.save()
 

commit 229545534242ac814a8f5c94d31c4b2894be67c7

add test coverage for command-line interface entry point
- fails due to bug introduced in e8eb7431f5e0f4a4ae8e471b5c0614428e505e80

try-examples.py
@@ -3,6 +3,7 @@
 import glob
 import pathlib
 import sys
+import subprocess
 
 import dmengine
 
@@ -18,3 +19,9 @@ print('run', [pathlib.Path(__file__).name] + sys.argv[1:])
 for filename in glob.glob(EXAMPLES):
     print('', f'dmengine.calculate({filename!r}, directory={DIRECTORY!r}, pdf={PDF!r})', sep='\n')
     analysis = dmengine.calculate(filename, directory=DIRECTORY, pdf=PDF)
+
+    cmd = [sys.executable, '-m', 'dmengine', filename, DIRECTORY]
+    if PDF:
+        cmd.append('--pdf')
+    print('', f'subprocess.run({cmd!r})', sep='\n')
+    subprocess.run(cmd, check=True)